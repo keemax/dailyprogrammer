34e472d1efd2205bfb7a344269c46f1aa402d525
Max Keener <maxnkeener@gmail.com>
2014-03-26 19:15:46 -0700

    fixed bug with check tiles more than once, refactored into methods

--- intermediate/119/sp.py
+++ intermediate/119/sp.py
@@ -7,47 +7,50 @@ http://www.reddit.com/r/dailyprogrammer/comments/17jvoh/013013_challenge_119_int
 import queue
 import sys
 
-gridSize = int(input())
+def main():
+	gridSize = int(input())
+	world = [gridSize * [0] for i in range(gridSize)]
+	q = queue.Queue()
 
-world = [gridSize * [0] for i in range(gridSize)]
-startPos = ()
-endPos = ()
+	startPos = ()
 
-for i in range(gridSize):
-	worldRow = list(input())
-	world[i] = worldRow
-	for j in range(gridSize):
-		if worldRow[j] == 'S':
-			startPos = (i, j, 0)
+	for i in range(gridSize):
+		worldRow = list(input())
+		world[i] = worldRow
+		for j in range(gridSize):
+			if worldRow[j] == 'S':
+				startPos = (i, j, 0)
 
-q = queue.Queue()
-q.put(startPos)
-found = False
+	q.put(startPos)
+	while not q.empty():
+		curr = q.get()
+		if curr[0] > 0:
+			north = (curr[0] - 1, curr[1], curr[2] + 1)
+			checkTile(north, world, q)
 
-while not q.empty():
-	curr = q.get()
-	if world[curr[0]][curr[1]] == 'E':
-		print("True, ", curr[2])
+		if curr[1] < gridSize - 1:
+			east = (curr[0], curr[1] + 1, curr[2] + 1)
+			checkTile(east, world, q)
+
+		if curr[0] < gridSize - 1:
+			south = (curr[0] + 1, curr[1], curr[2] + 1)
+			checkTile(south, world, q)
+
+		if curr[1] > 0:
+			west = (curr[0], curr[1] - 1, curr[2] + 1)
+			checkTile(west, world, q)
+
+	print("no path found")
+
+
+def checkTile(tile, world, q):
+	if world[tile[0]][tile[1]] == '.':
+			world[tile[0]][tile[1]] = 'Q'
+			q.put(tile)	
+	elif world[tile[0]][tile[1]] == 'E':
+		print("True, " + str(tile[2]))
 		sys.exit()
-	world[curr[0]][curr[1]] = 'W'
-	if curr[0] > 0:
-		north = (curr[0] - 1, curr[1], curr[2] + 1)
-		if world[north[0]][north[1]] != 'W':
-			q.put(north)
 
-	if curr[1] < gridSize - 1:
-		east = (curr[0], curr[1] + 1, curr[2] + 1)
-		if world[east[0]][east[1]] != 'W':
-			q.put(east)
 
-	if curr[0] < gridSize - 1:
-		south = (curr[0] + 1, curr[1], curr[2] + 1)
-		if world[south[0]][south[1]] != 'W':
-			q.put(south)
-
-	if curr[1] > 0:
-		west = (curr[0], curr[1] - 1, curr[2] + 1)
-		if world[west[0]][west[1]] != 'W':
-			q.put(west)
-
-print("no path found")
+if __name__ == "__main__":
+	main()
